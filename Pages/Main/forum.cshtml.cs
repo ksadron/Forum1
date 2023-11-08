e8290497b1e89b40667225cfd6517117bd425102
Ksadron <tomasz5sowa@gmail.com>
2023-11-08 13:08:37 +0100

    Dodanie forum

---
+++ Pages/Main/forum.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Forum.Main
+{
+    public class forumModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
